5e71dfa6e566cddd24a189c6871f04d7dfdd4250
AryaJadhav303 <132922399+AryaJadhav303@users.noreply.github.com>
2023-05-30 20:18:29 +0530

    Add files via upload

---
+++ Assignment_1_Area of circle.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 12:27:28 2023
+
+@author: Arya
+"""
+
+#write a program which accepts the radius of a circle from the user and computes area
+
+print('To calculate area of circle\nEnter value of radius:')
+r=float(input())
+
+A=(r**2)*(22/7)
+
+print('The area of circle with radius',r,'units is',A)
+
